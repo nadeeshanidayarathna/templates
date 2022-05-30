f9de864de290ecf3b832c56f99e431821006413e
raveedranPirarthanan <99256021+raveedranPirarthanan@users.noreply.github.com>
2022-05-30 08:43:09 +0530

    Feature/un  ug  reg  the 176 (#9)
    * scriptExclude_SP--SG--REG--L3OF28DONVAT
    
    * issueDateFix_SP--SG--REG--L3OF28DONVAT
    
    * titleUpdate_SP--SG--REG--L3OF28DONVAT
    
    * level7_SP--DGDSYFDP--REG--ROYAL-DECREE-NO
    
    * testRun_SP--SG--REG--L3OF28DONVAT
    
    * testRun_SP--DGDSYFDP--REG--ROYAL-DECREE-NO
    
    * rootLevel_SP--SG--REG--ACT-NO
    
    * level3_SP--SG--REG--ACT-NO
    
    * testRun_SP--SG--REG--ACT-NO
    
    * test2_SP--DGDSYFDP--REG--ROYAL-DECREE-NO
    
    * test2_SP--SG--REG--ACT-NO
    
    * testRun_AU--ASICA--REG--RG-83
    
    * testRun_MO--SDEDCSLCFS--REG--LAW-NO-0
    
    * testRun_MO--MG--REG--LAW-NO-1
    
    * QA_issueFix_UN--UG--REG--THE-176
    
    * QA_fix_rootScope_UN--UG--REG--THE-176
    
    * QA_fix_UN--UG--REG--THE-136
    
    * latestChanges
    
    * level2Fix_UN--UG--REG--THE-176
    
    * contentFix_SP--DGDSYFDP--REG--ROYAL-DECREE-NO
    
    * testRun_CA--CG--REG--INC-TAX-REG-CRC-C-945
    
    * CA--CG--REG--INC-TAX-REG-CRC-C-945_aTagfix
    
    * UN--UG--REG--THE-490_testRun
    
    * emptyLevelfix_CA--CG--REG--INC-TAX-REG-CRC-C-945
    
    * unwatedContentFix_CA--CG--REG--INC-TAX-REG-CRC-C-945
    
    * CA--CG--REG--INC-TAX-REG-CRC-C-945_level4Fix
    
    * CA--CG--REG--INC-TAX-REG-CRC-C-945_pseudoElementFix

---
+++ tests/CA--CG--REG--INC-TAX-REG-CRC-C-945.test.js
@@ -0,0 +1,134 @@
+const fs = require("fs");
+const sha1 = require("js-sha1");
+const line = '-'.repeat(process.stdout.columns);
+const test = require("../tests/base.test");
+
+
+async function htmlToText(browser, htmlPath, textPath, rootScope, removeSelectors, runBuildHtmlOnlyTest) {
+    {
+        console.log(line);
+        if (rootScope.includes(",")) {
+            throw "[TEST]:single root - failed!!! please check the root scopes whether it contains multiple selectors which is not allowed.";
+        } else {
+            console.log("[TEST]:single root - success!");
+        }
+
+        const page = await browser.newPage();
+        const contentHtml = fs.readFileSync(htmlPath, "utf8");
+        await page.setContent(contentHtml, { waitUntil: "domcontentloaded", timeout: 0 });
+        await page.waitForSelector(rootScope);
+        page.on("console", (msg) => console.log(msg.text()));
+        var tags = await page.evaluate(function process(rootScope, removeSelectors, line, runBuildHtmlOnlyTest) {
+            var tags = [];
+            function ToText(tags, node) {
+                const childNodes = node.childNodes;
+                for (childNode of childNodes) {
+                    ToText(tags, childNode);
+                }
+                if (node.nodeType == Node.TEXT_NODE && node.textContent.trim() != "") {
+                    const words = node.textContent.trim().split(" ");
+                    for (word of words) {
+                        if (word != "") {
+                            tags.push(word + "\n");
+                        }
+                    }
+                }
+            }
+            for (removeSelector of removeSelectors) {
+                Array.prototype.forEach.call(document.querySelectorAll(removeSelector), function (node) { node.remove(); });
+            }
+            for (node of document.querySelectorAll(rootScope)) {
+                ToText(tags, node);
+            }
+
+            if (runBuildHtmlOnlyTest) {
+                {
+                    // level test
+                    const levels = document.querySelectorAll(".level1,.level2,.level3,.level4,.level5,.level6,.level7,.level8,.level9,.level10");
+                    var levelNumbers = [];
+                    for (const level of levels) {
+                        levelNumbers.push(Number(level.className.replaceAll("level", "")));
+                    }
+                    var previousLevelNumber = 0;
+                    var levelOrderIssue = false;
+                    for (const levelNumber of levelNumbers) {
+                        if (levelNumber > previousLevelNumber + 1) {
+                            levelOrderIssue = true;
+                            break;
+                        }
+                        previousLevelNumber = levelNumber;
+                    }
+                    console.log(line);
+                    if (levelOrderIssue) {
+                        throw "[TEST]:level order - failed!!! please check the build HTML level order.";
+                    } else {
+                        console.log("[TEST]:level order - success!");
+                    }
+                }
+
+                {
+                    // image test
+                    const images = document.querySelectorAll("img");
+                    var imagePathIssue = false;
+                    for (const image of images) {
+                        if (!image.src.toLocaleUpperCase().startsWith("HTTP")) {
+                            imagePathIssue = true;
+                        }
+                    }
+                    console.log(line);
+                    if (imagePathIssue) {
+                        throw "[TEST]:image path - failed!!! please check the build HTML image src whether its an absolute link.";
+                    } else {
+                        console.log("[TEST]:image path - success!");
+                    }
+                }
+
+                {
+                    // anchor test
+                    const anchors = document.querySelectorAll("a[href]");
+                    var anchorPathIssue = false;
+                    for (const anchor of anchors) {
+                        if (!anchor.href.toLocaleUpperCase().startsWith("HTTP")) {
+                            anchorPathIssue = true;
+                        }
+                    }
+                    console.log(line);
+                    if (anchorPathIssue) {
+                        throw "[TEST]:anchor path - failed!!! please check the build HTML anchor href whether its an absolute link.";
+                    } else {
+                        console.log("[TEST]:anchor path - success!");
+                    }
+                }
+            }
+
+            return Promise.resolve(tags);
+        }, rootScope, removeSelectors, line, runBuildHtmlOnlyTest);
+
+        const content = tags.join("").trim().replace(/\n/g, "").replace(/ /g, "");
+        const hash = sha1(content);
+        tags.push(hash);
+        await test.write(textPath, tags);
+        await page.close();
+        return hash;
+    }
+}
+
+async function runTest(browser, originalHtmlPath, originalTextPath, buildHtmlPath, buildTextPath, originalRootScope, originalRemoveSelectors, buildRootScope, buildRemoveSelectors) {
+    const originalTextHash = await htmlToText(browser, originalHtmlPath, originalTextPath, originalRootScope, originalRemoveSelectors, false);
+
+    // ignoring root level optional attributes from test comparision
+    buildRemoveSelectors.push(".issue-date", ".effective-date");
+    const buildTextHash = await htmlToText(browser, buildHtmlPath, buildTextPath, buildRootScope, buildRemoveSelectors, true);
+
+    console.log(line);
+    if (originalTextHash != buildTextHash) {
+        throw "[TEST]:content hashes - failed!!! please compare the original HTML content and build HTML content." + " (original:" + originalTextHash + " build:" + buildTextHash + ")";
+    } else {
+        console.log("[TEST]:content hashes - success!" + " (original:" + originalTextHash + " build:" + buildTextHash + ")");
+    }
+    console.log(line);
+}
+
+// exports.write = write;
+exports.htmlToText = htmlToText;
+exports.runTest = runTest;
